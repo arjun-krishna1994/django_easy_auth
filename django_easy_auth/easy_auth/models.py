4936525eefe08ece7d420e99783586d77e128812
Arjun Krishna <arjuN@tapchief.com>
2016-09-17 13:16:22 +0530

    Added easy_auth to installed apps Created the basic JWT functions for encoding, decoding tokens along with functions for obtaining users from tokens and vice-versa Created models necessary to store the JWT's for a user

--- django_easy_auth/easy_auth/models.py
+++ django_easy_auth/easy_auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 from django.conf import settings
 AUTH_USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
+
+
+class UserJWToken(models.Model):
+    created = models.DateTimeField(auto_created=True, auto_now_add=True)
+    user = models.ForeignKey(AUTH_USER_MODEL)
+    token = models.TextField()
